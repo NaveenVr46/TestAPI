f9d80ddbc1051e5cad417d8b0ecaeef4290acf09
Naveen Prasad Ramasamy <94039083+NaveenVr46@users.noreply.github.com>
2025-03-09 12:10:20 +0530

    Add files via upload

---
+++ TestAPI.BAL/Interface/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestAPI.DAL;
+
+namespace TestAPI.BAL.Interface
+{
+    public interface IRecordRepository
+    {
+        Task<IEnumerable<Record>> GetUserAsync();
+        Task<int> InsertUserAsync(Record user);
+        Task<int> UpdateUserAsync(Record user);
+        Task<Record> GetUserByIdAsync(int id);
+        Task<bool> DeleteUserAsync(int id);
+
+    }
+}
---
+++ TestAPI.BAL/RepositoryAction.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using TestAPI.BAL.Interface;
+using TestAPI.DAL;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace TestAPI.BAL
+{
+    public class RepositoryAction : IRecordRepository
+    {
+        private readonly string _connectionString;
+
+        public RepositoryAction(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("APIConnection");
+        }
+
+        public async Task<IEnumerable<Record>> GetUserAsync()
+        {
+            using var connection = new SqlConnection(_connectionString);
+            string sql = "SELECT * FROM [Users]";
+            return await connection.QueryAsync<Record>(sql);
+        }
+
+        public async Task<int> InsertUserAsync(Record user)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            string sql = "INSERT INTO Users (FirstName, LastName, Email) VALUES (@FirstName, 'test', @Email);";
+            return await connection.ExecuteAsync(sql, new { user.FirstName, user.Email });
+        }
+        public async Task<int> UpdateUserAsync(Record user)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            string sql = "UPDATE Users SET FirstName = @FirstName, Email = @Email WHERE Id = @Id;";
+            return await connection.ExecuteAsync(sql, new { user.Id, user.FirstName, user.Email });
+        }
+        public async Task<Record?> GetUserByIdAsync(int id)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            string sql = "SELECT * FROM User WHERE Id = @Id";
+            return await connection.QueryFirstOrDefaultAsync<Record>(sql, new { Id = id });
+        }
+
+        public async Task<bool> DeleteUserAsync(int id)
+        {
+            using var connection = new SqlConnection(_connectionString); 
+            string sql = "DELETE FROM Users WHERE Id = @Id";
+            int Isdeleted = await connection.ExecuteAsync(sql, new { Id = id });
+            return Isdeleted > 0;
+        }
+
+    }
+}
