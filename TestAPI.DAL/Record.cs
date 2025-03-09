9fe07e64867c05cd5adffce1f6e4f269f3a80bb1
Naveen Prasad Ramasamy <94039083+NaveenVr46@users.noreply.github.com>
2025-03-09 12:09:20 +0530

    Add files via upload

---
+++ TestAPI.DAL/Record.cs
@@ -0,0 +1,12 @@
+﻿namespace TestAPI.DAL
+{
+    public class Record
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string? Password { get; set; }
+        public string? Name { get; set; }
+    }
+}
